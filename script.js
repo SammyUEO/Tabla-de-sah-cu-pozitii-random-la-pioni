b52a2d0a11dbf924eac91ff661a747a080314f62
SammyUEO <samuel.pop@emanuel.ro>
2024-04-11 09:50:48 +0300

    desenare tabla de sah

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let boardSize = 8; 
+let tileSize; 
+let pieces = []; 
+
 function setup() {
-  
+    createCanvas(400, 400); 
+    tileSize = width / boardSize; 
+
+   
 }
+
+function draw() {
+    
+    background(255); 
+    drawBoard();
+}
+
+function drawBoard() {
+    for (let y = 0; y < boardSize; y++) {
+        for (let x = 0; x < boardSize; x++) {
+            if ((x + y) % 2 === 0) {
+                fill("white"); 
+            } else {
+                fill("black"); 
+            }
+            rect(x * tileSize, y * tileSize, tileSize, tileSize);
+        }
+    }
+}
+
+
